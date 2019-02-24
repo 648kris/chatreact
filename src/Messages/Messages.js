d581c9660de5a3e03a6a37839a6b5daee971c800
Kristen <kristen3119@gmail.com>
2019-02-23 21:58:06 -0500

    messages drawer

--- src/Messages/Messages.js
+++ src/Messages/Messages.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import './messages.css';
-import '../style.css'
+import '../style.css';
 import List from './List';
+import MessagesDrawer from '../Drawer/messagesDrawer';
 import Conversation from './Conversation';
 
 class Messages extends Component {
   render(){
     return(
-      <div className="tealBackground">
-          <List />
-          <Conversation />
+      <div>
+        <MessagesDrawer />
       </div>
     )
   }
